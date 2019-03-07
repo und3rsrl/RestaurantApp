082d498b8894b4e60a44e4280d94adb702ba901b
alexchiurtu <alex.chiurtu@gmail.com>
2019-03-07 21:34:32 +0200

    Styled the MasterDetail Page

--- RestaurantApp/RestaurantApp/RestaurantApp/Views/User/Views/FoodMenuPage.xaml.cs
+++ RestaurantApp/RestaurantApp/RestaurantApp/Views/User/Views/FoodMenuPage.xaml.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace RestaurantApp.Views.User.Helpers
+namespace RestaurantApp.Views.User.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CoffeePage : ContentPage
+	public partial class FoodMenuPage : ContentPage
 	{
-		public CoffeePage ()
+		public FoodMenuPage ()
 		{
 			InitializeComponent ();
 		}
--- RestaurantApp/RestaurantApp/RestaurantApp/Views/User/Views/UserMasterPage.xaml.cs
+++ RestaurantApp/RestaurantApp/RestaurantApp/Views/User/Views/UserMasterPage.xaml.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace RestaurantApp.Views.User.Helpers
+namespace RestaurantApp.Views.User.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UserMasterPage : ContentPage
