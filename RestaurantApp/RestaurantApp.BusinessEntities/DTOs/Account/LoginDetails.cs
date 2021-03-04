54e61570ba86587c33946b541d0426547a81e33b
Alex Chiurtu <alex.chiurtu@gmail.com>
2021-03-04 20:29:25 +0200

    Moved DTOs to BussinesEntities

---
+++ RestaurantApp/RestaurantApp.BusinessEntities/DTOs/Account/LoginDetails.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantApp.BusinessEntities.DTOs.Account
+{
+    public class LoginDetails
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ RestaurantApp/RestaurantApp.BusinessEntities/DTOs/Account/RegisterDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantApp.BusinessEntities.DTOs.Account
+{
+    public class RegisterDetails
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "PASSWORD_MIN_LENGTH", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
